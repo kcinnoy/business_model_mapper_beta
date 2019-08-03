c66efd6660d0d7549d2adb6d1340bcb1c9137f57
kcinnoy <yonnick00@gmail.com>
2019-08-03 17:54:45 +0100

    added controllers

---
+++ business_model_mapper/app/controllers/strategic_goals_controller.rb
@@ -0,0 +1,13 @@
+class StrategicGoalsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
--- business_model_mapper/db/schema.rb
+++ business_model_mapper/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_154800) do
+ActiveRecord::Schema.define(version: 2019_08_03_163406) do
 
   create_table "businesses", force: :cascade do |t|
     t.string "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_08_03_154800) do
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
