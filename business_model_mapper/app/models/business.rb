9a72553fa1e9b9f065a044255f81486891a85af2
kcinnoy <yonnick00@gmail.com>
2019-08-08 00:17:05 +0100

    update business model to create default strat goal objects

--- business_model_mapper/app/models/business.rb
+++ business_model_mapper/app/models/business.rb
@@ -4,6 +4,25 @@ class Business < ApplicationRecord
   has_many :strategic_goals
   has_many :resources
 
+  after_save do
+    self.strategic_goals.create(
+      [
+        {
+        name: "SG1",
+        description: "SG1 description"
+        },
+        {
+        name: "SG2",
+        description: "SG2 description"
+        },
+        {
+        name: "SG3",
+        description: "SG3 description"
+        }
+      ]
+    )
+  end
+
   # accepts_nested_attributes_for :strategic_goals
 
 end
