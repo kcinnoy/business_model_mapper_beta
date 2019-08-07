369d3b250cc263965e8cbbff55bfed192115f203
kcinnoy <yonnick00@gmail.com>
2019-08-07 01:43:16 +0100

    confifg devise

--- business_model_mapper/app/controllers/application_controller.rb
+++ business_model_mapper/app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    helper_method :current_user, :logged_in?
+    before_action :authenticate_user!, except: [:home, :about, :contact]
     
-    def current_user
-      current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
-    end
+    # def current_user
+    #   current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
+    # end
   
-    def logged_in?
-      !!current_user
-    end
+    # def logged_in?
+    #   !!current_user
+    # end
 end
