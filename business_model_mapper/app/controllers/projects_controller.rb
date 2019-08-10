3ecb7a5c99b0581101dbb449905f6a2d806bf66a
kcinnoy <yonnick00@gmail.com>
2019-08-10 16:23:20 +0100

    added project resource nested form

--- business_model_mapper/app/controllers/projects_controller.rb
+++ business_model_mapper/app/controllers/projects_controller.rb
@@ -1,20 +1,35 @@
 class ProjectsController < ApplicationController
 
+    def Index
+        if params[:strategic_goal_id]
+            @projects = StrategicGoal.find(params[:strategic_goal_id]).projects
+        else
+            @projects = Project.all
+        end
+    end
+    
+    
     def new
-        @project = Project.new
+        @project = Project.new(strategic_goal_id: params[:strategic_goal_id])
+        @strategic_goal = StrategicGoal.find(params[:strategic_goal_id])
         2.times {@project.resources.build}
-        @strategic_goal = StrategicGoal.find(params[:strategic_id])
+   
     end
 
 
     def create
-      
+       
+        # raise params.inspect
+
         @project = Project.new(project_params)
-        @project.strategic_goal = StrategicGoal.find(params[:project][:strategic_goal][:id])
+        @strategic_goal = @project.strategic_goal_id
+
+      
+        # @project.strategic_goal = StrategicGoal.find(params[:project][:strategic_goal][:id])
         if @project.save
-          redirect_to project_path(@project)
+          redirect_to strategic_goal_project_path(@strategic_goal,@project)
         else
-          redirect_to project_path
+          redirect_to strategic_goal_project_path
         end
     end 
 
@@ -26,7 +41,16 @@ class ProjectsController < ApplicationController
     private
 
     def project_params
-        params.require(:project).permit(:name, :id, resources_attributes:[:name,:resource_type, :total_cost_year, :qty ], strategic_goals_attributes:[:id]) 
+        params.require(:project).permit(:name, :strategic_goal_id,
+            resources_attributes:[:name,:resource_type, :total_cost_year, :qty ]) 
+    end
+
+    def nested_project_params
+        params.require(:strategic_goal).permit(:name, :id,
+            project_attributes:[:name,
+                resources_attributes:[:name,:resource_type, :total_cost_year, :qty]
+            ]
+        )
     end
 
 end
--- business_model_mapper/config/routes.rb
+++ business_model_mapper/config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
   
   resources :businesses
-  resources :strategic_goals, only: [:new, :create, :show]
-  resources :projects, only: [:new, :create, :show]
+  resources :strategic_goals, only: [:new, :create, :show] do
+    resources :projects, only: [:index, :new, :create, :show]
+  end
   resources :resources, only: [:new, :create, :show]
 
   # resources :users, only: [:new, :create, :show]
